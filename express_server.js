7904499f8d450431623c72fd9d46cd2d31588db8
vagrant <kiel.strang@gmail.com>
2017-09-12 19:10:04 +0000

    Add EJS route handler for urls_index template

--- express_server.js
+++ express_server.js
@@ -2,10 +2,17 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
-var urlDatabase = {
+app.set("view engine", "ejs");
+
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls", (request, response) => {
+  let templateVars = { urls: urlDatabase };
+  response.render("urls_index", templateVars);
+});
 
 app.get("/", (request, response) => {
   response.end("Hello!");
