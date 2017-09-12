7009b0c4e6a5e989411fe5a089947cb3496ceceb
vagrant <kiel.strang@gmail.com>
2017-09-12 19:55:31 +0000

    Add form to request new shortened URL

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ var PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -14,6 +17,10 @@ app.get("/urls", (request, response) => {
   response.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (request, response) => {
+  response.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, url: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
@@ -21,6 +28,11 @@ app.get("/urls/:id", (req, res) => {
 
 app.get("/urls.json", (request, response) => {
   response.json(urlDatabase);
+});
+
+app.post("/urls", (request, response) => {
+  console.log(request.body);
+  response.send("Ok");
 });
 
 app.listen(PORT, () => {
