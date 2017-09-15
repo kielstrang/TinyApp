4667f188098a804dda0878e5001bad5a3356df3f
vagrant <kiel.strang@gmail.com>
2017-09-15 23:05:57 +0000

    Move short URL existence checking into error handling system

--- routes/error-handlers.js
+++ routes/error-handlers.js
@@ -5,7 +5,14 @@ module.exports = [
     if(error.name === 'RequestDataMissing') {
       return res.status(400).send(`Error: ${error.message}`);
     }
-    next();
+    next(error);
+  },
+
+  (error, req, res, next) => {
+    if(error.name === 'BadURLID') {
+      return res.status(404).redirect('/urls/notfound');
+    }
+    next(error);
   },
 
   (error, req, res, next) => {
